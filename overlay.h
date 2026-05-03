2615423c8dd2691021d1f0f2edec0b66fb77394f
ascii-leo <officialbulletbeast@gmail.com>
2026-05-03 19:09:14 +0100

    Update overlay.h

--- overlay.h
+++ overlay.h
@@ -10,40 +10,13 @@ private:
 	RECT target_rect{};
 	const char* process_name_internal = nullptr;
 	//DWORD window_extended_styles = {};
-
-
 	void locate_process() {
 		HWND window_handle = FindWindowA(NULL, process_name_internal);
 		GetWindowThreadProcessId(window_handle, &this->process_identifier);
 	};
-
-
-	uintptr_t get_module_base_address(_In_ const char* module_name, _In_ DWORD process_id) {
-
-		uintptr_t base_address = 0;
-		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, process_id);
-
-		if (snapshot != INVALID_HANDLE_VALUE) {
-			MODULEENTRY32 me32;
-			me32.dwSize = sizeof(me32);
-
-			if (Module32First(snapshot, &me32)) {
-				do {
-					if (_stricmp(me32.szModule, module_name) == 0) {
-						base_address = (uintptr_t)me32.modBaseAddr;
-						break;
-					}
-				} while (Module32Next(snapshot, &me32));
-			}
-			CloseHandle(snapshot);
-		}
-		return base_address;
-	}
-
-
 public:
 	bool occluded = true;
-	external_overlay(const char* process_name, const char* base_module_name, HWND window) : process_name_internal(process_name)  {
+	external_overlay(const char* process_name, HWND window) : process_name_internal(process_name)  {
 		this->locate_process();
 		auto get_external_window = GetForegroundWindow();
 		DWORD external_process_id = 0; GetWindowThreadProcessId(get_external_window, &external_process_id);
